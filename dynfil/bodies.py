c2d4dcc61bd89560ebd91071acd5227a4c581793
Raphael Michel <mail@raphaelmichel.de>
2016-12-18 12:26:43 +0100

    Move more complexity from main to BodyTrajectory

--- dynfil/bodies.py
+++ dynfil/bodies.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from .utils.angles import matrix_from_euler_xyz
+from .utils.parser import parse_trajectory
 
 
 class BodyTrajectory(object):
@@ -20,6 +21,9 @@ class BodyTrajectory(object):
             raise ValueError("Position and angle trajectories need to be of same length.")
         self.traj_pos = traj_pos
         self.traj_ort = np.array([matrix_from_euler_xyz(a, "123") for a in traj_angles])
+
+    def load_trajectory(self, filename):
+        self.set_trajectories(*parse_trajectory(filename))
 
     def to_constraint(self, time_index):
         return self.id, self.body_point, self.traj_pos[time_index], self.traj_ort[time_index]
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import numpy as np
 import rbdl
 
 from dynfil import constants, zmp
 from dynfil.bodies import BodyTrajectory
-from dynfil.utils.parser import parse_trajectory
 from dynfil.utils.plot import plot_trajectories
 
 model = rbdl.loadModel('data/models/iCubHeidelberg01_no_weights.urdf')
@@ -12,23 +10,21 @@ model = rbdl.loadModel('data/models/iCubHeidelberg01_no_weights.urdf')
 q_ini = constants.POSE_HALF_SITTING
 
 chest = BodyTrajectory(model, model.GetBodyId("chest"))
-chest.set_trajectories(*parse_trajectory('data/traj/com_traj.csv'))
+chest.load_trajectory('data/traj/com_traj.csv')
 
 lsole = BodyTrajectory(model, model.GetBodyId("l_sole"))
-lsole.set_trajectories(*parse_trajectory('data/traj/l_foot_traj.csv'))
+lsole.load_trajectory('data/traj/l_foot_traj.csv')
 
 rsole = BodyTrajectory(model, model.GetBodyId("r_sole"))
-rsole.set_trajectories(*parse_trajectory('data/traj/r_foot_traj.csv'))
+rsole.load_trajectory('data/traj/r_foot_traj.csv')
 
 traj_zmp = zmp.calculate_zmp_trajectory(model, q_ini, chest, lsole, rsole, sampling_interval=0.01)
 
 
-"""
 plot_trajectories(
-    trajectories=[traj_com_pos, traj_lsole_pos, traj_rsole_pos],
+    trajectories=[chest.traj_pos, lsole.traj_pos, rsole.traj_pos],
     labels=['CoM', 'Left foot', 'Right foot'],
     filename='out/trajectories.png',
-    rotations=[traj_com_ort, traj_lsole_pos, traj_rsole_pos],
+    rotations=[chest.traj_ort, lsole.traj_ort, rsole.traj_ort],
     show=True
 )
-"""
