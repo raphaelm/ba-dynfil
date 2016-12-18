b230d028328b9d608105981055244e434e92b724
Raphael Michel <mail@raphaelmichel.de>
2016-12-18 12:41:06 +0100

    Further (unsuccessful) tries on 3D orientation plot

--- dynfil/utils/plot.py
+++ dynfil/utils/plot.py
@@ -5,17 +5,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib as mpl
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import expm3
 
 
-def rotate_vector(vec, x, y, z):
-    res = []
-    for _x, _y, _z in zip(x, y, z):
-        Mx = expm3(np.cross(np.eye(3), np.array([1,0,0])* _x * np.pi / 180))
-        My = expm3(np.cross(np.eye(3), np.array([0,1,0])* _y * np.pi / 180))
-        Mz = expm3(np.cross(np.eye(3), np.array([0,0,1])* _z * np.pi / 180))
-        res.append(np.dot(Mz, np.dot(My, np.dot(Mx, vec))))
-    return np.array(res)
+def rotate_vector(vec, matarr):
+    res = np.zeros((len(matarr), 3))
+    vec = np.transpose([vec])
+    for i, mat in enumerate(matarr):
+        res[i] = np.dot(mat, vec)[:,0]
+    return res
 
 
 def plot_trajectories(trajectories, labels, filename, rotations=None, show=False):
@@ -29,11 +26,11 @@ def plot_trajectories(trajectories, labels, filename, rotations=None, show=False
 
     """
     if rotations:
-        rotsteps = 15
+        rotsteps = 30
         for traj, rots in zip(trajectories, rotations):
-            ux = rotate_vector(np.array([1, 0, 0]), rots[::rotsteps,0], rots[::rotsteps,1], rots[::rotsteps,2])
-            uy = rotate_vector(np.array([0, 1, 0]), rots[::rotsteps,0], rots[::rotsteps,1], rots[::rotsteps,2])
-            uz = rotate_vector(np.array([0, 0, 1]), rots[::rotsteps,0], rots[::rotsteps,1], rots[::rotsteps,2])
+            ux = rotate_vector(np.array([1, 0, 0]), rots[::rotsteps])
+            uy = rotate_vector(np.array([0, 1, 0]), rots[::rotsteps])
+            uz = rotate_vector(np.array([0, 0, 1]), rots[::rotsteps])
             ax.quiver(traj[::rotsteps,0], traj[::rotsteps,1], traj[::rotsteps,2], ux[:,0], ux[:,1], ux[:,2],
                       length=0.05, pivot='tail', color='red')
             ax.quiver(traj[::rotsteps,0], traj[::rotsteps,1], traj[::rotsteps,2], uy[:,0], uy[:,1], uy[:,2],
