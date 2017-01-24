639e1a17caa2f2fdb6083348016c0fcee0f353ee
Raphael Michel <r.michel@stud.uni-heidelberg.de>
2017-01-24 14:23:50 +0100

    compare-kevin.py: draw reference CoM

--- compare-kevin.py
+++ compare-kevin.py
@@ -10,8 +10,10 @@ model = rbdl.loadModel(filename)
 
 data_kevin = load_from_meshup('data/pgdata4_q_output_kevin.csv')
 data_dynfil = load_from_meshup('out/inverse_from_pg.csv')
+pgdata = np.genfromtxt('data/pgdata4.txt', delimiter=' ', dtype=None)
+timesteps = pgdata[:, 0]
 
-assert len(data_dynfil) == len(data_kevin)
+assert len(data_dynfil) == len(data_kevin) == len(pgdata)
 
 com_kevin = np.zeros((len(data_kevin), 3))
 com_dynfil = np.zeros((len(data_dynfil), 3))
@@ -29,13 +31,14 @@ plot.plot_trajectories_from_top(
     trajectories=[
         plot.PlotTrajectory(positions=com_kevin, rotations=None, label='Kevin', color='r'),
         plot.PlotTrajectory(positions=com_dynfil, rotations=None, label='Raphael', color='c'),
+        plot.PlotTrajectory(positions=pgdata[:, 1:3], rotations=None, label='PG reference', color='k'),
     ],
     filename=os.path.join('out', 'com_compare_kevin.pdf'),
     title='CoM trajectories'
 )
 
 plot.plot_q_values(
-    data_dynfil[:, 1],
+    timesteps,
     (data_kevin[:, 1:], data_dynfil[:, 1:]),
     labels=('Kevin', 'Raphael'),
     filename=os.path.join('out', 'q_compare_kevin.pdf'),
