d6fa1a299419e200a6176ee39de081b597fb2dff
Raphael Michel <r.michel@stud.uni-heidelberg.de>
2017-01-31 17:03:34 +0100

    Implement CoM calculation in PG

--- patterngenerator.py
+++ patterngenerator.py
@@ -1,16 +1,19 @@
-import numpy as np
-import rbdl
 import os
 
+import numpy as np
+
 from dynfil.utils import plot
+from dynfil.utils.linalg import tridag
 
 STEP_LENGTH = 0.2172 * 0.7  # 70% of foot length
 STEP_SINGLE_SUPPORT_TIME = 0.7
 STEP_DOUBLE_SUPPORT_TIME = 0.7
 FOOT_DISTANCE = 0.5
-TIME_LENGTH = 6 * (STEP_SINGLE_SUPPORT_TIME + STEP_DOUBLE_SUPPORT_TIME) + 1
+N_STEPS = 6
 RESOLUTION = 0.001
 COM_HEIGHT = 0.444239
+GRAVITY = 9.81
+WAIT_TIME = 0.5
 
 
 def interpol_foot_xy(time_length, final_pos):
@@ -18,9 +21,9 @@ def interpol_foot_xy(time_length, final_pos):
     coef[0] = 0.
     coef[1] = 0.
     coef[2] = 0.
-    coef[3] = (10. * final_pos)/(time_length**3)
-    coef[4] = (- 15. * final_pos)/(time_length**4)
-    coef[5] = (6. * final_pos)/(time_length**5)
+    coef[3] = (10. * final_pos) / (time_length ** 3)
+    coef[4] = (- 15. * final_pos) / (time_length ** 4)
+    coef[5] = (6. * final_pos) / (time_length ** 5)
     p = np.polynomial.Polynomial(coef)
     return p(np.arange(0, time_length, RESOLUTION))
 
@@ -29,14 +32,16 @@ def interpol_foot_z(time_length, height):
     coef = np.zeros(5)
     coef[0] = 0.
     coef[1] = 0.
-    coef[2] = (16 * height)/(time_length**2)
-    coef[3] = (-32 * height)/(time_length**3)
-    coef[4] = (16 * height)/(time_length**4)
+    coef[2] = (16 * height) / (time_length ** 2)
+    coef[3] = (-32 * height) / (time_length ** 3)
+    coef[4] = (16 * height) / (time_length ** 4)
     p = np.polynomial.Polynomial(coef)
     return p(np.arange(0, time_length, RESOLUTION))
 
 
-timesteps = np.arange(0, TIME_LENGTH, RESOLUTION)
+timesteps = np.arange(0,
+                      WAIT_TIME + (N_STEPS + 1) * (STEP_SINGLE_SUPPORT_TIME + STEP_DOUBLE_SUPPORT_TIME) + 1,
+                      RESOLUTION)
 lfoot = np.zeros((len(timesteps), 3))
 rfoot = np.zeros((len(timesteps), 3))
 zmp = np.zeros((len(timesteps), 3))
@@ -49,14 +54,17 @@ current_lfoot = lfoot[0]
 current_rfoot = rfoot[0]
 single_support_timesteps = int(STEP_SINGLE_SUPPORT_TIME / RESOLUTION)
 double_support_timesteps = int(STEP_DOUBLE_SUPPORT_TIME / RESOLUTION)
+wait_steps = int(WAIT_TIME / RESOLUTION)
 
-while t < len(timesteps) - 2 * (single_support_timesteps + double_support_timesteps):
+t += wait_steps
 
+# Generate foot and ZMP trajectories
+while t < wait_steps + len(timesteps) - 3 * (single_support_timesteps + double_support_timesteps):
     # Step with right foot
     rfoot[t:t + single_support_timesteps + 1, 2] = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
     rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + interpol_foot_xy(
         STEP_SINGLE_SUPPORT_TIME,
-        STEP_LENGTH if t > 0 else STEP_LENGTH / 2  # First step is a half step
+        STEP_LENGTH if t > wait_steps else STEP_LENGTH / 2  # First step is a half step
     )
     lfoot[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
     zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
@@ -68,8 +76,8 @@ while t < len(timesteps) - 2 * (single_support_timesteps + double_support_timest
     # Shift ZMP
     lfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
     rfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
-    zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
-    zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_rfoot[1]
+    zmp[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
+    zmp[t:t + double_support_timesteps, 1] = np.ones(double_support_timesteps) * current_rfoot[1]
     t += double_support_timesteps
 
     # Step with left foot
@@ -88,10 +96,28 @@ while t < len(timesteps) - 2 * (single_support_timesteps + double_support_timest
     # Shift ZMP
     lfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
     rfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
-    zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
-    zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_lfoot[1]
+    zmp[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
+    zmp[t:t + double_support_timesteps, 1] = np.ones(double_support_timesteps) * current_lfoot[1]
     t += double_support_timesteps
 
+# Move feed together, last step
+rfoot[t:t + single_support_timesteps + 1, 2] = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
+rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + interpol_foot_xy(
+    STEP_SINGLE_SUPPORT_TIME,
+    STEP_LENGTH / 2
+)
+lfoot[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
+zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
+zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_lfoot[1]
+t += single_support_timesteps
+current_rfoot = rfoot[t - 1]
+
+# Shift ZMP to middle
+lfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
+rfoot[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
+zmp[t:t + double_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
+zmp[t:t + double_support_timesteps, 1] = np.zeros(double_support_timesteps)
+t += double_support_timesteps
 
 timesteps = timesteps[:t]
 lfoot = lfoot[:t]
@@ -99,12 +125,24 @@ rfoot = rfoot[:t]
 zmp = zmp[:t]
 zmp[:, 2] = np.zeros(t)
 
+#  Calculate CoM Trajectories  (def (4.67) from p 140 in Kajita's book)
+b = np.ones(t) * (2 * COM_HEIGHT / (GRAVITY * RESOLUTION ** 2) + 1)
+a = c = np.ones(t - 1) * - COM_HEIGHT / (GRAVITY * RESOLUTION ** 2)
+b[0] += a[0]
+b[t-1] += c[-1]
+
+# Solve eq. (4.69) from p 141 in Kajita's book
+com = tridag(a, b, c, zmp)
+com[:, 2] = np.ones(t) * COM_HEIGHT
+
+# Plots
 plot.plot_trajectories_1d_axis(
     timesteps[:t],
     trajectories=[
         plot.PlotTrajectory(positions=lfoot, rotations=None, label='left foot', color='r'),
         plot.PlotTrajectory(positions=rfoot, rotations=None, label='right foot', color='g'),
         plot.PlotTrajectory(positions=zmp, rotations=None, label='ZMP', color='k'),
+        plot.PlotTrajectory(positions=com, rotations=None, label='CoM', color='b'),
     ],
     filename=os.path.join('out', 'pg_trajectories_over_time.pdf'),
     title='Raw trajectories'
@@ -115,6 +153,7 @@ plot.plot_trajectories(
         plot.PlotTrajectory(positions=lfoot, rotations=None, label='Left foot', color='r'),
         plot.PlotTrajectory(positions=rfoot, rotations=None, label='Right foot', color='g'),
         plot.PlotTrajectory(positions=zmp, rotations=None, label='ZMP', color='k'),
+        plot.PlotTrajectory(positions=com, rotations=None, label='CoM', color='b'),
     ],
     filename=os.path.join('out', 'pg_trajectories_3d.pdf'),
     title='Trajectories'
