8d15e2de4641049d1dcbe2fae25c374ac039f46e
Raphael Michel <r.michel@stud.uni-heidelberg.de>
2017-02-06 10:35:49 +0100

    PG: derivatives of lfoot, rfoot

--- patterngenerator.py
+++ patterngenerator.py
@@ -16,6 +16,13 @@ GRAVITY = 9.81
 WAIT_TIME = 0.5
 
 
+def poly_derivate(coef):
+    dcoef = np.zeros(len(coef) - 1)
+    for i in range(len(coef) - 1):
+        dcoef[i] = (i + 1) * coef[i + 1]
+    return dcoef
+
+
 def interpol_foot_xy(time_length, final_pos):
     coef = np.zeros(6)
     coef[0] = 0.
@@ -25,7 +32,18 @@ def interpol_foot_xy(time_length, final_pos):
     coef[4] = (- 15. * final_pos) / (time_length ** 4)
     coef[5] = (6. * final_pos) / (time_length ** 5)
     p = np.polynomial.Polynomial(coef)
-    return p(np.arange(0, time_length, RESOLUTION))
+
+    dcoef = poly_derivate(coef)
+    dp = np.polynomial.Polynomial(dcoef)
+
+    ddcoef = poly_derivate(dcoef)
+    ddp = np.polynomial.Polynomial(ddcoef)
+
+    return (
+        p(np.arange(0, time_length, RESOLUTION)),
+        dp(np.arange(0, time_length, RESOLUTION)),
+        ddp(np.arange(0, time_length, RESOLUTION))
+    )
 
 
 def interpol_foot_z(time_length, height):
@@ -36,7 +54,18 @@ def interpol_foot_z(time_length, height):
     coef[3] = (-32 * height) / (time_length ** 3)
     coef[4] = (16 * height) / (time_length ** 4)
     p = np.polynomial.Polynomial(coef)
-    return p(np.arange(0, time_length, RESOLUTION))
+
+    dcoef = poly_derivate(coef)
+    dp = np.polynomial.Polynomial(dcoef)
+
+    ddcoef = poly_derivate(dcoef)
+    ddp = np.polynomial.Polynomial(ddcoef)
+
+    return (
+        p(np.arange(0, time_length, RESOLUTION)),
+        dp(np.arange(0, time_length, RESOLUTION)),
+        ddp(np.arange(0, time_length, RESOLUTION))
+    )
 
 
 timesteps = np.arange(0,
@@ -45,6 +74,12 @@ timesteps = np.arange(0,
 lfoot = np.zeros((len(timesteps), 3))
 rfoot = np.zeros((len(timesteps), 3))
 zmp = np.zeros((len(timesteps), 3))
+dlfoot = np.zeros((len(timesteps), 3))
+drfoot = np.zeros((len(timesteps), 3))
+dzmp = np.zeros((len(timesteps), 3))
+ddlfoot = np.zeros((len(timesteps), 3))
+ddrfoot = np.zeros((len(timesteps), 3))
+ddzmp = np.zeros((len(timesteps), 3))
 lfoot[:, 1] = np.ones(len(timesteps)) * FOOT_DISTANCE / 2.
 rfoot[:, 1] = np.ones(len(timesteps)) * - FOOT_DISTANCE / 2.
 
@@ -56,16 +91,23 @@ single_support_timesteps = int(STEP_SINGLE_SUPPORT_TIME / RESOLUTION)
 double_support_timesteps = int(STEP_DOUBLE_SUPPORT_TIME / RESOLUTION)
 wait_steps = int(WAIT_TIME / RESOLUTION)
 
+# Interpolation
+i_z = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
+i_xy = interpol_foot_xy(STEP_SINGLE_SUPPORT_TIME, STEP_LENGTH)
+i_xy_half = interpol_foot_xy(STEP_SINGLE_SUPPORT_TIME, STEP_LENGTH / 2)
+
 t += wait_steps
 
 # Generate foot and ZMP trajectories
 while t < wait_steps + len(timesteps) - 3 * (single_support_timesteps + double_support_timesteps):
     # Step with right foot
-    rfoot[t:t + single_support_timesteps + 1, 2] = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
-    rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + interpol_foot_xy(
-        STEP_SINGLE_SUPPORT_TIME,
-        STEP_LENGTH if t > wait_steps else STEP_LENGTH / 2  # First step is a half step
-    )
+    r_i_xy = i_xy if t > wait_steps else i_xy_half
+    rfoot[t:t + single_support_timesteps + 1, 2] = i_z[0]
+    drfoot[t:t + single_support_timesteps + 1, 2] = i_z[1]
+    ddrfoot[t:t + single_support_timesteps + 1, 2] = i_z[2]
+    rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + r_i_xy[0]
+    drfoot[t:t + single_support_timesteps + 1, 0] = r_i_xy[1]
+    ddrfoot[t:t + single_support_timesteps + 1, 0] = r_i_xy[2]
     lfoot[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
     zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
     zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_lfoot[1]
@@ -81,11 +123,12 @@ while t < wait_steps + len(timesteps) - 3 * (single_support_timesteps + double_s
     t += double_support_timesteps
 
     # Step with left foot
-    lfoot[t:t + single_support_timesteps + 1, 2] = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
-    lfoot[t:t + single_support_timesteps + 1, 0] = current_lfoot[0] + interpol_foot_xy(
-        STEP_SINGLE_SUPPORT_TIME,
-        STEP_LENGTH
-    )
+    lfoot[t:t + single_support_timesteps + 1, 2] = i_z[0]
+    dlfoot[t:t + single_support_timesteps + 1, 2] = i_z[1]
+    ddlfoot[t:t + single_support_timesteps + 1, 2] = i_z[2]
+    lfoot[t:t + single_support_timesteps + 1, 0] = current_lfoot[0] + i_xy[0]
+    dlfoot[t:t + single_support_timesteps + 1, 0] = i_xy[1]
+    ddlfoot[t:t + single_support_timesteps + 1, 0] = i_xy[2]
     rfoot[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
     zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_rfoot[0]
     zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_rfoot[1]
@@ -101,11 +144,12 @@ while t < wait_steps + len(timesteps) - 3 * (single_support_timesteps + double_s
     t += double_support_timesteps
 
 # Move feed together, last step
-rfoot[t:t + single_support_timesteps + 1, 2] = interpol_foot_z(STEP_SINGLE_SUPPORT_TIME, 0.02)
-rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + interpol_foot_xy(
-    STEP_SINGLE_SUPPORT_TIME,
-    STEP_LENGTH / 2
-)
+rfoot[t:t + single_support_timesteps + 1, 2] = i_z[0]
+drfoot[t:t + single_support_timesteps + 1, 2] = i_z[1]
+ddrfoot[t:t + single_support_timesteps + 1, 2] = i_z[2]
+rfoot[t:t + single_support_timesteps + 1, 0] = current_rfoot[0] + i_xy[0]
+drfoot[t:t + single_support_timesteps + 1, 0] = i_xy[1]
+ddrfoot[t:t + single_support_timesteps + 1, 0] = i_xy[2]
 lfoot[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
 zmp[t:t + single_support_timesteps, 0] = np.ones(double_support_timesteps) * current_lfoot[0]
 zmp[t:t + single_support_timesteps, 1] = np.ones(double_support_timesteps) * current_lfoot[1]
@@ -121,7 +165,11 @@ t += double_support_timesteps
 
 timesteps = timesteps[:t]
 lfoot = lfoot[:t]
+dlfoot = dlfoot[:t]
+ddlfoot = ddlfoot[:t]
 rfoot = rfoot[:t]
+drfoot = drfoot[:t]
+ddrfoot = ddrfoot[:t]
 zmp = zmp[:t]
 zmp[:, 2] = np.zeros(t)
 
@@ -136,12 +184,14 @@ com = tridag(a, b, c, zmp)
 com[:, 2] = np.ones(t) * COM_HEIGHT
 
 # Save to file
-filedata = np.zeros((t, 23))
+filedata = np.zeros((t, 29))
 filedata[:, 0] = timesteps
 filedata[:, 1:4] = com
 filedata[:, 7:10] = lfoot
 filedata[:, 13:16] = rfoot
 filedata[:, 19:22] = zmp
+filedata[:, 22:25] = dlfoot
+filedata[:, 25:28] = drfoot
 np.savetxt('out/pg_data.txt', filedata, delimiter=' ')
 
 
@@ -156,6 +206,16 @@ plot.plot_trajectories_1d_axis(
     ],
     filename=os.path.join('out', 'pg_trajectories_over_time.pdf'),
     title='Raw trajectories'
+)
+
+plot.plot_trajectories_1d_axis(
+    timesteps[:t],
+    trajectories=[
+        plot.PlotTrajectory(positions=dlfoot, rotations=None, label='left foot dot', color='r'),
+        plot.PlotTrajectory(positions=drfoot, rotations=None, label='right foot dot', color='g'),
+    ],
+    filename=os.path.join('out', 'pg_derivs.pdf'),
+    title='Derivatives'
 )
 
 plot.plot_trajectories(
